316226c2c6da8a5be5eed9ef3490ac444679c320
Luis Eduardo Braschi <luis.eduardo.braschi@outlook.com>
2021-12-09 12:34:52 -0300

    `arti_psn_group_notices_by_min_amount` as a filter, not option

--- includes/class-shipping-notices.php
+++ includes/class-shipping-notices.php
@@ -76,6 +76,7 @@ class Shipping_Notices {
 			$min_amount = $settings['min_amount'];
 
 			if( wc_string_to_bool( get_option( 'arti_psn_group_notices_by_min_amount', 'no' ) ) ){
+			$group_by_min_amount = apply_filters( 'arti_psn_group_notices_by_min_amount', 'no' );
 
 				if( !isset( $location_groups[$min_amount] ) ){
 					$location_groups[$min_amount]['expanded_locations'] = [];
