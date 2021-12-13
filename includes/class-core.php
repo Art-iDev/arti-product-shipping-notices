9ba30d29b531e44fe8e33d5576c2e438153db18f
Luis Eduardo Braschi <luis.eduardo.braschi@outlook.com>
2021-12-13 17:03:00 -0300

    Adding transient removal when vendor shipping is updated

--- includes/class-core.php
+++ includes/class-core.php
@@ -5,7 +5,14 @@ namespace Arti\PSN;
 class Core {
 
 	public function __construct(){
+
 		add_shortcode( 'shipping-notice', [ Shipping_Notices::get_instance(), 'render_notices' ] );
+		add_action( 'wcfm_vendor_shipping_settings_update', [ $this, 'update_vendor_notice_cache' ] );
+
+	}
+
+	public function update_vendor_notice_cache( $vendor_id ){
+		delete_transient( 'arti_psn_shipping_notice_' . $vendor_id );
 	}
 }
 
